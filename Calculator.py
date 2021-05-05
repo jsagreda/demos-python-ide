607ecc55cb65176a27bfe1e7dd19bfff23897d2f
juanagreda <jsagreda@gmail.com>
2021-05-04 19:00:05 -0500

    Add calculator

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+a = 1
+b = 3
+c = 5
+
+
+def calculate_sum():
+    result = a + b + c
+
+    print("Sum of a, b, c is", result)
+
+    if result < 10:
+        print("The sum < 10")
+    else:
+        print("The sum is", result, ">= 10")
+
+# Calculate
+
+calculate_sum()
